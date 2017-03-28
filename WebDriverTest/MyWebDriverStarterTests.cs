9f62e68fb0acaa513487a5ec25e747170a53ca09
Tonko_I <Tonko_I@TONKO-I>
2017-03-28 12:33:11 +0300

    Add project files.

---
+++ WebDriverTest/MyWebDriverStarterTests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.IE;
+using System.Collections;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace WebDriverTest
+{
+    [TestClass]
+    public class MyWebDriverStarterTests
+    {
+        RemoteWebDriver Driver;
+        WeightCountPage page;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (Driver == null)
+            {
+                InternetExplorerOptions options = new InternetExplorerOptions();
+                options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+                options.RequireWindowFocus = true;
+
+                Driver = new InternetExplorerDriver(options);
+
+                
+                Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1000);
+                Driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(1000);
+            }
+            Driver.Navigate().GoToUrl(@"http://svyatoslav.biz/testlab/wt/index.php");
+            page = new WeightCountPage(Driver);
+        }
+
+        [TestMethod]
+        public void TestPageOpen()
+        {
+            try
+            {
+                var title = Driver.Title;
+                Assert.AreEqual("Расчёт веса", title);
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestInputEmpty()
+        {
+            try
+            {
+                page.clickSubmit();
+                Thread.Sleep(1000);
+                Assert.IsTrue(page.isTDExists("Не указано имя.\r\nРост должен быть в диапазоне 50-300 см.\r\nВес должен быть в диапазоне 3-500 кг.\r\nНе указан пол."));
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestInputName()
+        {
+            try
+            {
+                page.typeName("Temp");
+                page.clickSubmit();
+                Thread.Sleep(1000);
+                Assert.IsTrue(page.isTDExists("Рост должен быть в диапазоне 50-300 см.\r\nВес должен быть в диапазоне 3-500 кг.\r\nНе указан пол."));
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestInputCorrect()
+        {
+            try
+            {
+                page.typeName("T");
+                page.typeHeight("170");
+                page.typeWeight("65");
+                page.clickMGender();
+                page.clickSubmit();
+                Thread.Sleep(1000);
+                Assert.IsTrue(page.isTDExists("масса тела"));
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestHeightOutOfRangeHight()
+        {
+            try
+            {
+                page.typeName("T");
+                page.typeHeight("301");
+                page.typeWeight("65");
+                page.clickMGender();
+                page.clickSubmit();
+                Thread.Sleep(1000);
+                Assert.IsTrue(page.isTDExists("Рост должен быть в диапазоне 50-300 см."));
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestHeightOutOfRangeLow()
+        {
+            try
+            {
+                page.typeName("T");
+                page.typeHeight("29");
+                page.typeWeight("65");
+                page.clickMGender();
+                page.clickSubmit();
+                Thread.Sleep(1000);
+                Assert.IsTrue(page.isTDExists("Рост должен быть в диапазоне 50-300 см."));
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestWeightOutOfRangeHight()
+        {
+            try
+            {
+                page.typeName("T");
+                page.typeHeight("170");
+                page.typeWeight("501");
+                page.clickMGender();
+                page.clickSubmit();
+                Thread.Sleep(1000);
+                Assert.IsTrue(page.isTDExists("Вес должен быть в диапазоне 3-500 кг."));
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void TestWeightOutOfRangeLow()
+        {
+            try
+            {
+                page.typeName("T");
+                page.typeHeight("170");
+                page.typeWeight("2");
+                page.clickMGender();
+                page.clickSubmit();
+                Thread.Sleep(1000);
+                Assert.IsTrue(page.isTDExists("Вес должен быть в диапазоне 3-500 кг."));
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+    }
+}
---
+++ WebDriverTest/WeightCountPage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.IE;
+using System.Collections;
+using OpenQA.Selenium;
+
+namespace WebDriverTest
+{
+    public class WeightCountPage
+    {
+        private RemoteWebDriver driver;
+
+        By nameLocator = By.Name("name");
+        By heightLocator = By.Name("height");
+        By weightLocator = By.Name("weight");
+        By genderMLocator = By.CssSelector("input[value='m']");
+        By genderFLocator = By.CssSelector("input[value='f']");
+        By submitLocator = By.XPath("//input[@type='submit']");
+        By messageLocator = By.TagName("b");
+        By tdTag = By.TagName("td");
+
+
+
+        public WeightCountPage(RemoteWebDriver driver)
+        {
+            this.driver = driver;
+            if (!"Расчёт веса".Equals(driver.Title))
+            {
+                throw new Exception("This is not the weight count page");
+            }
+        }
+
+        public WeightCountPage typeName(String name)
+        {
+            driver.FindElement(nameLocator).SendKeys(name); 
+            return this;
+        }
+
+        public WeightCountPage typeHeight(String height)
+        {
+            driver.FindElement(heightLocator).SendKeys(height);
+            return this;
+        }
+
+        public WeightCountPage typeWeight(String weight)
+        {
+            driver.FindElement(weightLocator).SendKeys(weight); 
+            return this;
+        }
+
+        public WeightCountPage clickMGender()
+        {
+            driver.FindElement(genderMLocator).Click();
+            return this;
+        }
+
+        public WeightCountPage clickFGender()
+        {
+            driver.FindElement(genderFLocator).Click();
+            return this;
+        }
+
+        public WeightCountPage clickSubmit()
+        {
+            driver.FindElement(submitLocator).Click();
+            return this;
+        }
+
+        public string getMessage()
+        {
+            var el = driver.FindElement(messageLocator);
+           return driver.FindElement(messageLocator).Text;
+        }
+
+        public bool isTDExists(string value)
+        {
+            var el = driver.FindElements(tdTag);
+            return ((el.Where(x => System.Text.RegularExpressions.Regex.
+            IsMatch(x.Text, value, System.Text.RegularExpressions.RegexOptions.IgnoreCase))).Count() > 0);
+        }
+
+    }
+}
